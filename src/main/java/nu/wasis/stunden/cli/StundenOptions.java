7deb87e0980582a27359b1561bc5d480dae28bd8
Cornelius Lilge <cornelius.lilge@gmail.com>
2013-10-02 08:11:33 +0200

    Javadoc ftw!

--- src/main/java/nu/wasis/stunden/cli/StundenOptions.java
+++ src/main/java/nu/wasis/stunden/cli/StundenOptions.java
@@ -1,13 +1,28 @@
 package nu.wasis.stunden.cli;
 
+import nu.wasis.stunden.Stunden;
+
 import org.apache.commons.cli.Options;
 
+/**
+ * The commmand line {@link Options} for {@link Stunden}.
+ * 
+ * stunden currently has only one (required) option `c' which must point to the
+ * configuration file.
+ */
 public class StundenOptions extends Options {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * The only valid (and required) option for stunden. Must point to a
+     * readable, valid json-File.
+     */
     public static final String OPTION_CONFIG_FILE = "c";
 
+    /**
+     * Create a new {@link StundenOptions} object with pre-filled values.
+     */
     public StundenOptions() {
         addOptions();
     }
